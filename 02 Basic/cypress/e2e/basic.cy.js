bbffae574838fa665f9269eb4b0924288dffc4a4
AbolfazlAdhami <aadhami880@gmail.com>
2025-08-05 06:04:30 +0330

    :test_tube: test: test for task page and img titles

--- 02 Basic/cypress/e2e/basic.cy.js
+++ 02 Basic/cypress/e2e/basic.cy.js
@@ -1,5 +1,16 @@
-describe("template spec", () => {
-  it("passes", () => {
+/// <reference types="cypress" />
+
+describe("task page", () => {
+  it("should render the main image", () => {
     cy.visit("http://localhost:5173/");
+    cy.get("header img[alt='A list']").should("be.visible").should("have.attr", "src").and("include", "/logo.png");
+    cy.get("header").find("img").should("be.visible").and("have.attr", "src").and("include", "/logo.png");
+  });
+  it("Should display the page title", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get(".main-header h1").contains("React Tasks");
+    cy.get("h1").should("have.length", 1);
+    cy.get("h1").should("not.have.class", "error"); // Example of checking for a class
+    // cy.contains("React Tasks"); // Any Where in Page
   });
 });
