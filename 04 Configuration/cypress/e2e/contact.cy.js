f2034432256fc1733cafbb537c520a6a96f7def8
AbolfazlAdhami <aadhami880@gmail.com>
2025-08-19 06:06:02 +0330

    :white_check_mark: test: hooks commands and query task and config

--- 04 Configuration/cypress/e2e/contact.cy.js
+++ 04 Configuration/cypress/e2e/contact.cy.js
@@ -1,57 +1,81 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
-describe('contact form', () => {
-  it('should submit the form', () => {
-    cy.visit('http://localhost:5173/about');
-    cy.get('[data-cy="contact-input-message"]').type('Hello world!');
-    cy.get('[data-cy="contact-input-name"]').type('John Doe');
-    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
-      expect(el.attr('disabled')).to.be.undefined;
-      expect(el.text()).to.eq('Send Message');
-    });
-    cy.screenshot();
-    cy.get('[data-cy="contact-input-email"]').type('test@example.com{enter}');
-    // cy.get('[data-cy="contact-btn-submit"]')
-    //   .contains('Send Message')
-    //   .should('not.have.attr', 'disabled');
-    cy.screenshot();
-    cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
-    // cy.get('@submitBtn').click();
-    cy.get('@submitBtn').contains('Sending...');
-    cy.get('@submitBtn').should('have.attr', 'disabled');
+describe("contact form", () => {
+  before(() => {
+    // This is run once before all tests
+    cy.visit("/about");
+    cy.getById("contact-input-message").type("Hello world!");
+    cy.get("form").as("contactForm").should("be.visible");
+  });
+  beforeEach(() => {
+    // This is run before each test
+    cy.visit("/about");
+    cy.getById("contact-input-message").should("have.value", "");
+    cy.getById("contact-input-name").should("have.value", "");
+    cy.getById("contact-input-email").should("have.value", "");
   });
 
-  it('should validate the form input', () => {
-    cy.visit('http://localhost:5173/about');
-    cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
-      expect(el).to.not.have.attr('disabled');
-      expect(el.text()).to.not.equal('Sending...');
+  afterEach(() => {
+    // This is run after each test
+    cy.getById("contact-btn-submit").contains("Send Message").and("have.not.attr", "disabled");
+  });
+  after(() => {
+    // This is run once after all tests
+    cy.getById("contact-btn-submit").contains("Send Message").and("have.not.attr", "disabled");
+    cy.getById("contact-input-message").should("have.value", "");
+    cy.getById("contact-input-name").should("have.value", "");
+    cy.getById("contact-input-email").should("have.value", "");
+  });
+
+  it("should submit the form", () => {
+    cy.task("seedDatabase", "filename.csv").then((returnValue) => {
+      // ... use returnValue
     });
-    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
-    cy.get('[data-cy="contact-input-message"]').as('msgInput');
-    cy.get('@msgInput').focus().blur();
-    cy.get('@msgInput')
-      .parent()
-      .should((el) => {
-        expect(el.attr('class')).not.to.be.undefined;
-        expect(el.attr('class')).contains('invalid');
-      })
+    cy.getById("contact-input-message").type("Hello world!");
+    cy.getById("contact-input-name").type("John Doe");
+    cy.getById("contact-btn-submit").then((el) => {
+      expect(el.attr("disabled")).to.be.undefined;
+      expect(el.text()).to.eq("Send Message");
+    });
+    cy.screenshot();
+    cy.getById("contact-input-email").type("test@example.com ");
+    cy.submitForm();
+    cy.screenshot();
+    cy.getById("contact-btn-submit").as("submitBtn");
+    cy.get("@submitBtn").contains("Sending...");
+    cy.get("@submitBtn").should("have.attr", "disabled");
+  });
 
-    cy.get('[data-cy="contact-input-name"]').focus().blur();
-    cy.get('[data-cy="contact-input-name"]')
+  it("should validate the form input", () => {
+    cy.submitForm();
+    cy.getById("contact-btn-submit").then((el) => {
+      expect(el).to.not.have.attr("disabled");
+      expect(el.text()).to.not.equal("Sending...");
+    });
+    cy.getById("contact-btn-submit").contains("Send Message");
+    cy.getById("contact-input-message").as("msgInput");
+    cy.get("@msgInput").focus().blur();
+    cy.get("@msgInput")
       .parent()
       .should((el) => {
-        expect(el.attr('class')).not.to.be.undefined;
-        expect(el.attr('class')).contains('invalid');
-      })
+        expect(el.attr("class")).not.to.be.undefined;
+        expect(el.attr("class")).contains("invalid");
+      });
 
-    cy.get('[data-cy="contact-input-email"]').focus().blur();
-    cy.get('[data-cy="contact-input-email"]')
+    cy.getById("contact-input-name").focus().blur();
+    cy.getById("contact-input-name")
       .parent()
       .should((el) => {
-        expect(el.attr('class')).not.to.be.undefined;
-        expect(el.attr('class')).contains('invalid');
-      })
+        expect(el.attr("class")).not.to.be.undefined;
+        expect(el.attr("class")).contains("invalid");
+      });
+
+    cy.getById("contact-input-email").focus().blur();
+    cy.getById("contact-input-email")
+      .parent()
+      .should((el) => {
+        expect(el.attr("class")).not.to.be.undefined;
+        expect(el.attr("class")).contains("invalid");
+      });
   });
 });
